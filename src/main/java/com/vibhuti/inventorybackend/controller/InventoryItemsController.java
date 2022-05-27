657a88db7d6ed3ae49fc54a16bcd0001e224018b
Vibhuti Maheshwari <vibhutimaheshwari03@gmail.com>
2022-05-27 21:41:20 +0530

    Update item API created

--- src/main/java/com/vibhuti/inventorybackend/controller/InventoryItemsController.java
+++ src/main/java/com/vibhuti/inventorybackend/controller/InventoryItemsController.java
@@ -1,8 +1,11 @@
 package com.vibhuti.inventorybackend.controller;
 
+import com.vibhuti.inventorybackend.exception.ResourceNotFoundException;
 import com.vibhuti.inventorybackend.model.InventoryItems;
 import com.vibhuti.inventorybackend.repositories.InventoryItemsRepo;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +13,7 @@ import java.util.List;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 //@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {GET,PUT, POST, DELETE})
+@Log4j2
 @RestController
 @RequestMapping("api/v1")
 public class InventoryItemsController {
@@ -20,7 +24,19 @@ public class InventoryItemsController {
     //To fetch list of all items in inventory
     @GetMapping("/all-inventory-items")
     public List<InventoryItems> getAllItems(){
+        log.info("GET - Fetching all items in inventory");
         return inventoryItemsRepo.findAll();
+    }
+
+    //To fetch list of items by productName
+    @GetMapping("/inventory-items/{productName:[a-zA-z &,]*}")
+    public List<InventoryItems> getItemByName(
+            @PathVariable("productName")
+            String productName){
+        log.info("GET - Fetching items with name: " +productName);
+        List<InventoryItems> item = inventoryItemsRepo.findByProductName(productName);
+
+        return item;
     }
 
     //To add an item in the inventory
@@ -28,6 +44,34 @@ public class InventoryItemsController {
     public InventoryItems addItem(
             @RequestBody
             InventoryItems inventoryItem){
+        log.info("POST - Adding inventory item" +inventoryItem.getProductName());
         return inventoryItemsRepo.save(inventoryItem);
+    }
+
+    //To update an item
+    @PutMapping("/update-item/{id}")
+    public ResponseEntity<InventoryItems> updateInventoryItem(
+            @PathVariable
+            Long id,
+            @RequestBody
+            InventoryItems itemDetails
+    ){
+        InventoryItems item = inventoryItemsRepo
+                                .findById(id)
+                                .orElseThrow(() -> new ResourceNotFoundException("No product with id:" +id));
+
+        item.setProductName(itemDetails.getProductName());
+        item.setProductQuantity(itemDetails.getProductQuantity());
+        item.setCostPrice(itemDetails.getCostPrice());
+        item.setSellPrice(itemDetails.getSellPrice());
+
+        InventoryItems updatedItem = inventoryItemsRepo.save(item);
+
+        log.info("PUT - Updating inventory item " +updatedItem.getProductName()+
+                "with quantity:"+updatedItem.getProductQuantity()+
+                "and cost price:"+updatedItem.getCostPrice()+
+                "and selling price:"+updatedItem.getSellPrice());
+
+        return ResponseEntity.ok(updatedItem);
     }
 }
--- src/main/java/com/vibhuti/inventorybackend/model/InventoryItems.java
+++ src/main/java/com/vibhuti/inventorybackend/model/InventoryItems.java
@@ -1,13 +1,11 @@
 package com.vibhuti.inventorybackend.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
